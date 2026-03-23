11e38d0fe250410e6852b590ab34ed41902669b5
Rabiyev09 <komilrabiyev243@gmail.com>
2026-03-23 20:22:37 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let count = 1;
-while (count < 10) {
-    console.log(count);
-    count++;
-}
+// let count = 1;
+// while (count < 10) {
+//     console.log(count);
+//     count++;
+// }
 
 
 let count = 1;
@@ -17,63 +17,63 @@ while (count < 10) {
 
 console.log("Цикл завершен");
 
-let i = 1;
-do {
-    console.log(i);
-    i++;
-    while (i < 5);
-}
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+//     while (i < 5);
+// }
 
-let number;
-while (true) {
-    number = prompt("Введите число:");
-    if (number == 10) {
-        console.log("Вы ввели 10, цикл будет остановлен.");
-        break; // Выход из цикла, если число равно 10
-    }
-}
+// let number;
+// while (true) {
+//     number = prompt("Введите число:");
+//     if (number == 10) {
+//         console.log("Вы ввели 10, цикл будет остановлен.");
+//         break; // Выход из цикла, если число равно 10
+//     }
+// }
 
 for (let i = 1; i <= 10; i++) {
     console.log(i);
 }
 
-let i = 0;
-for (; ;) {
-    i++;
-    if (i > 100) {
-        break;
-    }
-    console.log(i);
-}
+// let i = 0;
+// for (; ;) {
+//     i++;
+//     if (i > 100) {
+//         break;
+//     }
+//     console.log(i);
+// }
 //6
 
-let numbers = [1, 2, 3, 4, 5];
-for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
-}
-//7
+// let numbers = [1, 2, 3, 4, 5];
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+// //7
 
-//while неизвестноеколичество итераций
-let password = "";
-while (password !== "12345") {
-    password = prompt("Введите пароль:");
-}
-// for известноеколичество итераций
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-}
+// //while неизвестноеколичество итераций
+// let password = "";
+// while (password !== "12345") {
+//     password = prompt("Введите пароль:");
+// }
+// // for известноеколичество итераций
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
 
-//8
-for (let i = 0; i < 5; i--) {
-    console.log(i);
-}
-//9
+// //8
+// for (let i = 0; i < 5; i--) {
+//     console.log(i);
+// }
+// //9
 
-let number = prompt("Введите число до 10:");
+// let number = prompt("Введите число до 10:");
 
-for (let i = 0; i < number; i++) {
-    if (i % 2 === 0) {
-        console.log(i);
-    }
-}
-//10
+// for (let i = 0; i < number; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+// //10
